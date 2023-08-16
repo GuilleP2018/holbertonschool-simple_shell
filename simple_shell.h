81d44c4bf1d9923c520e21dba286e177df9e5c49
GuilleP2018 <gpereyo@gmail.com>
2023-08-16 00:00:29 +0000

    this is the starting point of my header file, prototypes and structs coming soon

---
+++ simple_shell.h
@@ -0,0 +1,13 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#endif
