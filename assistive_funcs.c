12b6ea24aa69ed8f889427ede41dfdb0c05baa39
GuilleP2018 <gpereyo@gmail.com>
2023-08-24 18:28:38 +0000

    Betty fixes

---
+++ assistive_funcs.c
@@ -0,0 +1,41 @@
+#include "simple_shell.h"
+
+/**
+ * trim - removes whitespace
+ * @str: string given
+ */
+
+void trim(char *str)
+{
+	int start = 0;
+	int end = strlen(str) - 1;
+	int index = 0;
+	int i;
+
+	while (isspace(str[start]))
+		start++;
+
+	while (end >= 0 && isspace(str[end]))
+		end--;
+
+	for (i = start; i <= end; i++)
+		str[index++] = str[i];
+
+	str[index] = '\0';
+}
+
+/**
+ * free_array - Frees memory allocated for arrays
+ * @array: The array to be freed
+ * @n: The number of strings in the array
+ */
+void free_array(char **array, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		free(array[i]);
+	}
+	free(array);
+}
--- main.c
+++ main.c
@@ -82,7 +82,6 @@ void exec_command(char *command, char **env)
 /**
  * child_exec - Create a child process and execute the command
  * @tokens: The tokenized command and arguments
- * @full_path: path concat
  * @env: The environment variables
  */
 void child_exec(char **tokens, char **env)
@@ -152,50 +151,5 @@ char **get_path(void)
 	paths[num_paths] = NULL;
 	free(path_env);
 	return (paths);
-}
-
-/**
- * free_array - Frees memory allocated for arrays
- * @tokens: The array to be freed
- * @token_count: The number of strings in the array
- */
-void free_array(char **array, int n)
-{
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		free(array[i]);
-	}
-	free(array);
-}
-
-/**
- * trim - removes whitespace
- * @str: string given
- */
-
-void trim(char *str)
-{
-	int start = 0;
-	int end = strlen(str) - 1;
-	int index = 0;
-	int i;
-
-	while (isspace(str[start])) {
-		start++;
-	}
-
-	while (end >= 0 && isspace(str[end])) {
-		end--;
-	}
-
-
-	for (i = start; i <= end; i++)
-	{
-		str[index++] = str[i];
-	}
-
-	str[index] = '\0';
 }
 
